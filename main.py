46d55ca38e0a6f1a373c08aea814f2920b8df53f
hitruk <abakhtiev@mail.ru>
2023-08-31 22:10:59 +0300

    commit #3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from  tkinter import *
+from  data import Db
 
 #root = Tk()
 #root.title('hello world')
@@ -9,8 +10,17 @@ from  tkinter import *
 
 # Пока не будем использовать наследование, обойдемся композицией.
 	
+#p = Db()
+#print(p.__dict__)
+#sql = p.query_parent()
+#print(p.get_res(sql))
+
 class App():
+    p = Db()
+    sql = p.query_parent()
+    data = p.get_res(sql)
     def __init__(self, root):
+        # windows
         self.root = root
         self.root.title('hello world')
         self.root.geometry('650x400')
@@ -26,14 +36,15 @@ class App():
         # Listbox
         self.listbox = Listbox()
         self.listbox.pack(side=LEFT, fill=BOTH, expand=1)
-        [self.listbox.insert(END, i) for i in range(110)]
+        #[self.listbox.insert(END, i) for i in range(110)]
+        [self.listbox.insert(END, i) for i in self.data]
         # SCROLLBAR
         self.scroll = Scrollbar(orient="vertical", command = self.listbox.yview)
         self.scroll.pack(side=RIGHT, fill=Y)
         # для взаиможействия со scrollbar
         self.listbox.config(yscrollcommand=self.scroll.set)
-
-         
+    def op(self):
+        print(self.data) 
 
 root = Tk()
 app = App(root)
